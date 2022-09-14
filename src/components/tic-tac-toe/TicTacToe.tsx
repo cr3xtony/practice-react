4aa8a7a409834286e9b1a3e493c01df6a1a68d46
ton <tony@workspaceit.com>
2022-09-14 15:24:39 +0600

    added some css

--- src/components/tic-tac-toe/TicTacToe.tsx
+++ src/components/tic-tac-toe/TicTacToe.tsx
@@ -1,26 +1,53 @@
 import React from 'react';
+import { CrossIcon, CircleIcon } from './svg/icons';
 
 const TicTacToe = () => {
+  const [crossOrCircle, setCrossOrCircle] = React.useState('cross');
   return (
     <div className='tictactoe'>
+      <div className='now-playing'>
+        <div className='now-playing-header'>Now Playing</div>
+        <div className='now-playing-player'>Player 1</div>
+      </div>
       <div className='tictactoe-container'>
         <div className='tictactoe-row tictactoe-border-bottom'>
           <div className='tictactoe-cell tictactoe-border-right'>
-            <span>x</span>
+            {crossOrCircle === 'cross' ? <CrossIcon /> : <CircleIcon />}
           </div>
-          <div className='tictactoe-cell tictactoe-border-right'></div>
-          <div className='tictactoe-cell'></div>
+          <div className='tictactoe-cell tictactoe-border-right'>
+            {crossOrCircle === 'cross' ? <CrossIcon /> : <CircleIcon />}
+          </div>
+          <div className='tictactoe-cell'>
+            {crossOrCircle === 'cross' ? <CrossIcon /> : <CircleIcon />}
+          </div>
         </div>
         <div className='tictactoe-row tictactoe-border-bottom'>
-          <div className='tictactoe-cell tictactoe-border-right'></div>
-          <div className='tictactoe-cell tictactoe-border-right'></div>
-          <div className='tictactoe-cell'></div>
+          <div className='tictactoe-cell tictactoe-border-right'>
+            {crossOrCircle === 'cross' ? <CrossIcon /> : <CircleIcon />}
+          </div>
+          <div className='tictactoe-cell tictactoe-border-right'>
+            {crossOrCircle === 'cross' ? <CrossIcon /> : <CircleIcon />}
+          </div>
+          <div className='tictactoe-cell'>
+            {crossOrCircle === 'cross' ? <CrossIcon /> : <CircleIcon />}
+          </div>
         </div>
         <div className='tictactoe-row'>
-          <div className='tictactoe-cell tictactoe-border-right'></div>
-          <div className='tictactoe-cell tictactoe-border-right'></div>
-          <div className='tictactoe-cell'></div>
+          <div className='tictactoe-cell tictactoe-border-right'>
+            {crossOrCircle === 'cross' ? <CrossIcon /> : <CircleIcon />}
+          </div>
+          <div className='tictactoe-cell tictactoe-border-right'>
+            {crossOrCircle === 'cross' ? <CrossIcon /> : <CircleIcon />}
+          </div>
+          <div className='tictactoe-cell'>
+            {crossOrCircle === 'cross' ? <CrossIcon /> : <CircleIcon />}
+          </div>
         </div>
+      </div>
+
+      <div className='bottom-button'>
+        <button>New Game</button>
+        <button>Restart</button>
       </div>
     </div>
   );
